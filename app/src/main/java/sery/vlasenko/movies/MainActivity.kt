e76080998cf9535efc06d55e44b87685587e498b
Sery0077 <sery.vlasenko.ru@gmail.com>
2022-07-27 21:14:04 +0300

    add dependencies

--- app/src/main/java/sery/vlasenko/movies/MainActivity.kt
+++ app/src/main/java/sery/vlasenko/movies/MainActivity.kt
@@ -2,6 +2,7 @@ package sery.vlasenko.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.rxjava3.core.Observable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
